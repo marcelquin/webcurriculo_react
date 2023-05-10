0af0b4ea61211ca7d6a204f35135744106c79ba8
marcelquin <ursoquinteiro@gmail.com>
2023-05-10 08:17:38 -0400

    adição de Hover e link para certificados
    Adicionado Hover na div correspondente dos cursos, para posterior adição dos arquivos dos certificados, proximos passos escanear certificados e editalos adequadamente

--- app/src/pages/Cursos/Index.jsx
+++ app/src/pages/Cursos/Index.jsx
@@ -4,6 +4,7 @@ import Header from "../../componentes/Header/"
 import Perfil from "../../componentes/Perfil/"
 import Footer from '../../componentes/Footer/'
 
+
 const instFID ={
   inst: 'FID',
   curso: 'Baicharelado em  Sistema de Informação',
@@ -79,83 +80,101 @@ function Cursos() {
               <h3>{instFID.inst}</h3>
               <p>{instFID.curso}</p>
               <p>Ano: {instFID.ano}</p>
-            </div>                
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
+            </div> 
+
             <div className='blocoCurso'>
               <h3>{insudemy.inst}</h3>
               <p>{insudemy.curso}</p>
               <p>Ano: {insudemy.ano}</p>
-            </div>        
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
+            </div>  
+
             <div className='blocoCurso'>
               <h3>{insudemy.inst}</h3>
               <p>{insudemy.curso2}</p>
               <p>Ano: {insudemy.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             <div className='blocoCurso'>
               <h3>{insudemy.inst}</h3>
               <p>{insudemy.curso3}</p>
               <p>Ano: {insudemy.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             <div className='blocoCurso'>
               <h3>{instiped.inst}</h3>
               <p>{instiped.curso}</p>
               <p>Ano: {instiped.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instiped.inst}</h3>
               <p>{instiped.curso2}</p>
               <p>Ano: {instiped.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instbrava.inst}</h3>
               <p>{instbrava.curso}</p>
               <p>Ano: {instbrava.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instsoftblue.inst}</h3>
               <p>{instsoftblue.curso}</p>
               <p>Ano: {instsoftblue.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instensinon.inst}</h3>
               <p>{instensinon.curso}</p>
               <p>{instensinon.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             <div className='blocoCurso'>
               <h3>{instensinon.inst}</h3>
               <p>{instensinon.curso2}</p>
               <p>{instensinon.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
 
              <div className='blocoCurso'>
               <h3>{instFID.inst}</h3>
               <p>{instFID.curso2}</p>
               <p>Ano: {instFID.ano2}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instFID.inst}</h3>
               <p>{instFID.curso3}</p>
               <p>Ano: {instFID.ano2}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instFID.inst}</h3>
               <p>{instFID.curso4}</p>
               <p>Ano: {instFID.ano2}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div> 
             <div className='blocoCurso'>
               <h3>{instFID.inst}</h3>
               <p>{instFID.curso5}</p>
               <p>Ano: {instFID.ano2}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>          
             
             <div className='blocoCurso'>
               <h3>{inscurso24.inst}</h3>
               <p>{inscurso24.curso}</p>
               <p>Ano: {inscurso24.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             <div className='blocoCurso'>
               <h3>{inscurso24.inst}</h3>
               <p>{inscurso24.curso2}</p>
               <p>{inscurso24.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
 
             
@@ -164,11 +183,13 @@ function Cursos() {
               <h3>{instensinon.inst}</h3>
               <p>{instensinon.curso3}</p>
               <p>{instensinon.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             <div className='blocoCurso'>
               <h3>{instensinon.inst}</h3>
               <p>{instensinon.curso4}</p>
               <p>{instensinon.ano}</p>
+              <a href='#' target='_blank' className='faculdade'>Certificado</a>
             </div>
             
 
